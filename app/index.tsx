66a054f079b317474248f534ace0ddd002ad8302
swatantrasinha <swatantra.sinha@bt.com>
2026-04-10 11:55:40 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,72 @@
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  type todoType = {
+    id: number;
+    title: string;
+    isDone: boolean;
+  };
+  const todoData = [
+    {
+      id: 1,
+      title: "Todo 1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      isDone: false,
+    },
+    {
+      id: 4,
+      title: "Todo 4",
+      isDone: true,
+    },
+    {
+      id: 5,
+      title: "Todo 5",
+      isDone: false,
+    },
+    {
+      id: 6,
+      title: "Todo 6",
+      isDone: false,
+    },
+  ];
+
+  const displayTodo = (itemData: todoType) => {
+    return (
+      <View>
+        <Text>{itemData.title}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
+      <FlatList
+        data={todoData}
+        keyExtractor={(index) => index.id.toString()}
+        // renderItem={({ item }) => (
+        //   <View>
+        //     <Text>{item.title}</Text>
+        //   </View>
+        // )}
+        renderItem={({ item }) => displayTodo(item)}
+      />
+    </SafeAreaView>
+  );
+}
